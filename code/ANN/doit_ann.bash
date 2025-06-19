196b838619c49ce8df3cd61ec6bfa9a35cf29679
albedamm <s215067@dtu.dk>
2025-06-19 14:55:05 +0200

    Ran ANN

--- code/ANN/doit_ann.bash
+++ code/ANN/doit_ann.bash
@@ -4,7 +4,7 @@
 RDIR="/Users/alberteenglund/Documents/DTU/8_Semester/22125_algorithms_in_bioinformatics/algorithms/BioAlgoProject2025/code"
 
 ## Define path you where you have placed the HLA data sets
-DDIR="/Users/alberteenglund/Documents/DTU/8_Semester/22125_algorithms_in_bioinformatics/algorithms/BioAlgoProject2025/data/AllFiles"
+DDIR="/Users/alberteenglund/Documents/DTU/8_Semester/22125_algorithms_in_bioinformatics/algorithms/BioAlgoProject2025/data/all_files_new"
 
 # A0201 A0202 A1101 A3001 B0702 B1501 B5401
 # Here you can type your allele names
@@ -24,12 +24,12 @@ mkdir -p $sc
 
 cd $sc
 
-for i in 50 100 200
-do
-mkdir -p epocs.$i
-cd epocs.$i
+# for i in 50 100 200
+# do
+# mkdir -p epocs.$i
+# cd epocs.$i
 
-for nh in 3 5 10 15
+for nh in 3 5 10
 do 
 mkdir -p hidden.$nh
 cd hidden.$nh
@@ -44,22 +44,26 @@ for n in 1 2 3 4
 do
 
 # Do training
-if [ ! -f $a-$sc-$i-$nh-$epi-$n.syn ] 
+if [ ! -f $a-$sc-$nh-$epi-$n.syn ] 
 then
-	python "$RDIR/ANN/ANN_train.py" -sc $sc -t "$DDIR/$a/f00$n" -e "$DDIR/$a/c00$n" -syn $a-$sc-$i-$nh-$epi-$n.syn -stop | tee train.log.$n
+	python "$RDIR/ANN/ANN_train.py" -sc $sc -nh $nh -epi $epi -t "$DDIR/$a/f00$n.csv" -e "$DDIR/$a/c00$n.csv" -syn $a-$sc-$nh-$epi-$n.syn -stop | tee train.log.$n
 fi
 
 # Do evaluation
 if [ ! -f c00$n.pred ] 
 then
-	python "$RDIR/ANN/ANN_forward.py" -sc $sc -e "$DDIR/$a/c00$n" -syn $a-$sc-$i-$nh-$epi-$n.syn  | tee >(grep "PCC:" > c00$n.pcc) | grep -v "PCC:" > c00$n.pred
+	python "$RDIR/ANN/ANN_forward.py" -sc $sc -e "$DDIR/$a/c00$n.csv" -syn $a-$sc-$nh-$epi-$n.syn  | tee >(grep "PCC:" > c00$n.pcc) | grep -v "PCC:" > c00$n.pred
 fi
 
 done
 
+# Save concatenated predictions
+cat c00{1..4}.pred | grep -v "#" > concat.pred
+
 # Do concatinated evaluation
-echo $a $sc `cat c00{1..4}.pred | grep -v "#" | gawk '{print $2,$3}' | ../../../xycorr` \
-	   `cat c00{1..4}.pred | grep -v "#" | gawk '{print $2,$3}' | gawk 'BEGIN{n+0; e=0.0}{n++; e += ($1-$2)*($1-$2)}END{print "MSE:", e/n}' ` >> ../summary.txt
+echo $a $sc Hidden neurons: $nh Epsilon: $epi `cat c00{1..4}.pred | grep -v "#" | gawk '{print $2,$3}' | ../../../../../xycorr` \
+	   `cat c00{1..4}.pred | grep -v "#" | gawk '{print $2,$3}' | gawk 'BEGIN{n+0; e=0.0}{n++; e += ($1-$2)*($1-$2)}END{print "MSE:", e/n}' ` >> ../../../summary.txt
+
 
 cd ..
 
@@ -68,3 +72,15 @@ done
 cd ..
 
 done
+
+cd ..
+
+done
+
+cd ..
+
+done
+
+# cd ..
+
+# done
