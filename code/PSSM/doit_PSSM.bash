7bc6df7e85732941d5a837c80fe6127768f477ef
s215098 <s215098@dtu.dk>
2025-06-19 12:13:47 +0200

    changed doit_PSSM.bash

--- code/PSSM/doit_PSSM.bash
+++ code/PSSM/doit_PSSM.bash
@@ -7,11 +7,13 @@ RDIR="/Users/kristinetoftjohansen/Desktop/Algo/BioAlgoProject2025/code"
 DDIR="/Users/kristinetoftjohansen/Desktop/Algo/BioAlgoProject2025/data/AllFiles"
 
 # Here you can type your allele names
-for a in A0201 A3002 
+# A0201 A0202 A1101 A3001 B0702 B1501 B5401 B5701
+for a in A0201
 do
 
 # Create and move into a results directory for each allele.
-mkdir -p $a.res
+rm -rf $a.res # remove the previous created result files
+mkdir -p $a.res #-p is just mkdir options that ensures it is done "safely"
 cd $a.res
 
 # Here you can type the to test
@@ -23,7 +25,7 @@ mkdir -p b.$beta
 cd b.$beta
 
 # Loop over the 5 cross validation configurations
-for n in 0 1 2 3 4 
+for n in 0 1 2 3
 do
 
 # Do training
@@ -39,7 +41,8 @@ if [ ! -f c00$n.pred ] #if c00$n.pred file does not exist.
 then
 	# Use the learned matrix to score peptides from the test set.
     # Save predictions to c00n.pred (ignore lines starting with "PCC:").
-	python $RDIR/PSSM/pep2score.py -mat mat.$n -f  $DDIR/$a/c00$n | grep -v "PCC:" > c00$n.pred
+	# python $RDIR/PSSM/pep2score.py -mat mat.$n -f  $DDIR/$a/c00$n | grep -v "PCC:" > c00$n.pred
+	python $RDIR/PSSM/pep2score.py -mat mat.$n -f  $DDIR/$a/c00$n | tee >(grep "PCC:" > c00$n.pcc) | grep -v "PCC:" > c00$n.pred
 fi
 
 done
@@ -48,8 +51,8 @@ done
 # Print allele, lambda, followed by:
 # - calculated correlation between predicted and actual values (by xycorr)
 # - MSE (Mean Squared Error) over all predictions.
-echo $a $l `cat c00{0..4}.pred | grep -v "#" | gawk '{print $2,$3}' | ../../code/xycorr` \
-	   `cat c00{0..4}.pred | grep -v "#" | gawk '{print $2,$3}' | gawk 'BEGIN{n+0; e=0.0}{n++; e += ($1-$2)*($1-$2)}END{print e/n}' `
+echo $a $l `cat c00{0..3}.pred | grep -v "#" | gawk '{print $2,$3}' | /Users/kristinetoftjohansen/Desktop/Algo/BioAlgoProject2025/code/xycorr` \
+	   `cat c00{0..3}.pred | grep -v "#" | gawk '{print $2,$3}' | gawk 'BEGIN{n+0; e=0.0}{n++; e += ($1-$2)*($1-$2)}END{print "MSE:", e/n}' ` >> ../summary.txt
 
 cd ..
 
